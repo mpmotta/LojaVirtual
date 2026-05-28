37a695a20b55c4b1cafb4a38e576a8ca08fdf338
Márcio Motta <mpmotta@gmail.com>
2026-05-28 16:34:58 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,26 +1,36 @@
 <?php
-class Conexao {
+
+namespace App\Model;
+
+use PDO;
+use PDOException;
+
+class Conexao
+{
     private $host = "localhost";
     private $db_name = "loja";
     private $username = "root";
     private $password = "usbw";
     public $conn;
 
-    public function getConnection() {
-        // A trava de segurança que impede o erro no GitHub Actions
+    public function getConnection()
+    {
         if (getenv('GITHUB_ACTIONS')) {
             return null;
         }
 
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
+        } catch (PDOException $exception) {
             echo "Erro de conexão: " . $exception->getMessage();
         }
         return $this->conn;
     }
 }
-?>
