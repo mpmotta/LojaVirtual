67b9a82fafab3bc1e7f0c62259337b47bb2d3941
Márcio Motta <mpmotta@gmail.com>
2026-05-28 16:42:37 -0300

    setup tests

--- tests/ProdutoControllerTest.php
+++ tests/ProdutoControllerTest.php
@@ -1,44 +1,51 @@
 <?php
+
+namespace App\Tests;
+
 use PHPUnit\Framework\TestCase;
+use App\Controller\ProdutoController;
+use App\Model\Produto;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-class ProdutoControllerTest extends TestCase {
-
+class ProdutoControllerTest extends TestCase
+{
     // --- TESTES DO CONTROLLER ---
 
-    public function testConsultaRetornaArray() {
+    public function testConsultaRetornaArray()
+    {
         $controller = new ProdutoController();
         $this->assertIsArray($controller->consulta());
     }
 
     // --- TESTES DO MODEL (PRODUTO) ---
 
-    public function testProdutoPodeSerInstanciado() {
+    public function testProdutoPodeSerInstanciado()
+    {
         $produto = new Produto();
         $this->assertInstanceOf(Produto::class, $produto);
     }
 
-    public function testListarTodosRetornaArray() {
+    public function testListarTodosRetornaArray()
+    {
         $produto = new Produto();
         $this->assertIsArray($produto->listarTodos());
     }
 
-    public function testListarPorCategoriaRetornaArray() {
+    public function testListarPorCategoriaRetornaArray()
+    {
         $produto = new Produto();
-        // Testando com uma categoria genérica
         $resultado = $produto->listarPorCategoria('Eletrônicos');
         $this->assertIsArray($resultado);
     }
 
-    public function testBuscarPorIdRetornaArrayOuFalse() {
+    public function testBuscarPorIdRetornaArrayOuFalse()
+    {
         $produto = new Produto();
-        // Testando com um ID inválido (deve retornar array vazio ou false conforme seu código)
         $resultado = $produto->buscarPorId(999999);
-        $this->assertNotIsInstanceOf(PDOStatement::class, $resultado);
+        $this->assertIsArray($resultado);
     }
 
-    public function testBuscarPorNomeRetornaArray() {
+    public function testBuscarPorNomeRetornaArray()
+    {
         $produto = new Produto();
         $resultado = $produto->buscarPorNome('teste');
         $this->assertIsArray($resultado);
@@ -46,9 +53,9 @@ class ProdutoControllerTest extends TestCase {
 
     // --- TESTE DE SEGURANÇA (SQL INJECTION) ---
 
-    public function testBuscaPorNomeComCaracteresMaliciosos() {
+    public function testBuscaPorNomeComCaracteresMaliciosos()
+    {
         $produto = new Produto();
-        // Verifica se o sistema processa strings com aspas sem quebrar
         $resultado = $produto->buscarPorNome("' OR 1=1 --");
         $this->assertIsArray($resultado);
     }
