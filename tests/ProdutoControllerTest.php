bf3ab62c6c22dc14cf10af6ef786d6e073304efb
Márcio Motta <mpmotta@gmail.com>
2026-05-28 16:10:19 -0300

    Update ProdutoControllerTest.php

--- tests/ProdutoControllerTest.php
+++ tests/ProdutoControllerTest.php
@@ -1,13 +1,9 @@
 <?php
 use PHPUnit\Framework\TestCase;
-
-// Carrega as ferramentas do Composer (incluindo o PHPUnit e seu autoload)
 require_once __DIR__ . '/../vendor/autoload.php';
 
 class ProdutoControllerTest extends TestCase {
     public function testConsultaRetornaArray() {
-        // Como o classmap foi configurado, a classe ProdutoController 
-        // será carregada automaticamente pelo autoload
         $controller = new ProdutoController();
         $this->assertIsArray($controller->consulta());
     }
