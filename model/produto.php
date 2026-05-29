d1eca3e43dd3142444f927257078bc70c689be47
MARCIO PAIVA DA MOTTA <71364579049@senacrs.edu.br>
2026-05-29 11:18:54 -0300

    teste de correcao de padroes

--- model/produto.php
+++ model/produto.php
@@ -15,7 +15,7 @@ class Produto
         $this->conn = $database->getConnection();
     }
 
-    public function listar_todos()
+    public function listarTudo()
     {
         if ($this->conn === null) {
             return [];
